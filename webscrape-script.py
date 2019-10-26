1c1cc2cb2fdb808cf5b597af60aa868e55b55b84
Alan Tsui <alantsui18@gmail.com>
2019-10-25 23:39:37 -0700

    git sync

--- webscrape-script.py
+++ webscrape-script.py
@@ -28,12 +28,12 @@ def main():
 
     for submission in top_subreddit:
         topics_dict["title"].append(submission.title)
-        topics_dict["score"].append(submission.score)
-        topics_dict["id"].append(submission.id)
+        #topics_dict["score"].append(submission.score)
+        #topics_dict["id"].append(submission.id)
         topics_dict["url"].append(submission.url)
-        topics_dict["comms_num"].append(submission.num_comments)
-        topics_dict["created"].append(submission.created)
-        topics_dict["body"].append(submission.selftext)
+        #topics_dict["comms_num"].append(submission.num_comments)
+        #topics_dict["created"].append(submission.created)
+        #topics_dict["body"].append(submission.selftext)
 
     topics_data = pd.DataFrame(topics_dict)
 
