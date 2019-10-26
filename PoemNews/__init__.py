81e12212fb0f1e142e67948c69782588a5d5e8ba
Gary Kwong <gakwong1@berkeley.edu>
2019-10-26 02:02:16 -0700

    Static style changes

--- PoemNews/__init__.py
+++ PoemNews/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder='static', static_url_path='/static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         #DATABASE=os.path.join(app.instance_path, 'squeaker.sqlite'),
