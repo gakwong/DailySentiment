7a3798e578c7eb42fba709e117333e1f73d107bb
Gary Kwong <gakwong1@berkeley.edu>
2019-10-26 00:40:22 -0700

    New Changes

--- PoemNews/main.py
+++ PoemNews/main.py
@@ -3,6 +3,7 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 from werkzeug import secure_filename
+#import PoemNews.webscrape_script.py as wbs
 
 #app = Flask(__name__)
 bp = Blueprint('main', __name__)
@@ -10,6 +11,8 @@ bp = Blueprint('main', __name__)
 @bp.route("/")
 def main():
     #a = get database info
+    #dict = wbs.main()
+    #print(dict)
     return render_template('index.html')
 
 '''
